6c931adf9e1fff57eb4d271897210f9180787f08
rotimi <dd427118@ohio.edu>
2020-08-21 11:34:25 -0400

    Updating both files

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ It is well with my soul
 
 Information and exposure
 
+Build strength in one
+
 
 
 
--- try1.py
+++ try1.py
@@ -1,7 +1,10 @@
+
 #Second edit
 print("Hello World")
 
 print("You are just not smart")
+
+print("You are the man")
 
 
 
